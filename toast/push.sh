1d34d660bca25df58afaa078af8256b732ec3f87
(bot) Dipsy Wong <ycwongal@connect.ust.hk>
2024-11-17 13:47:44 +0800

    my own git push step

---
+++ toast/push.sh
@@ -0,0 +1,45 @@
+SOURCE_BEFORE_DIRECTORY="${1}"
+SOURCE_DIRECTORY="${2}"
+DESTINATION_GITHUB_USERNAME="${3}"
+DESTINATION_REPOSITORY_NAME="${4}"
+GITHUB_SERVER="${5}"
+USER_EMAIL="${6}"
+USER_NAME="${7}"
+DESTINATION_REPOSITORY_USERNAME="${8}"
+TARGET_BRANCH="${9}"
+COMMIT_MESSAGE="${10}"
+TARGET_DIRECTORY="${11}"
+CREATE_TARGET_BRANCH_IF_NEEDED="${12}"
+
+
+          source-directory: 'website'
+          destination-github-username: ${{ inputs.repo-user }}
+          destination-repository-name: ${{ inputs.repo-name }}
+          target-branch: main
+
+mkdir --parents "$HOME/.ssh"
+DEPLOY_KEY_FILE="$HOME/.ssh/deploy_key"
+echo "${SSH_DEPLOY_KEY}" > "$DEPLOY_KEY_FILE"
+chmod 600 "$DEPLOY_KEY_FILE"
+
+SSH_KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
+ssh-keyscan -H "$GITHUB_SERVER" > "$SSH_KNOWN_HOSTS_FILE"
+
+export GIT_SSH_COMMAND="ssh -i "$DEPLOY_KEY_FILE" -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_FILE"
+
+GIT_CMD_REPOSITORY="git@$GITHUB_SERVER:$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git"
+
+git lfs install
+
+git config --global user.email "$USER_EMAIL"
+git config --global user.name "$USER_NAME"
+
+ORIGIN_COMMIT="https://$GITHUB_SERVER/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
+COMMIT_MESSAGE="${COMMIT_MESSAGE/\$GITHUB_REF/$GITHUB_REF}"
+
+
+cd "$SOURCE_DIRECTORY"
+git add .
+git commit -m "$COMMIT_MESSAGE"
+git push "$GIT_CMD_REPOSITORY" "$TARGET_BRANCH"
